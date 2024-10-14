a0a4fb9cbf73127dbf160153d9b90e013a6e2f09
Liliia Parashchak <liliia.parashchak@gmail.com>
2024-10-14 20:37:45 +0300

    Tests completed

--- rustlings/exercises/tests/tests2.rs
+++ rustlings/exercises/tests/tests2.rs
@@ -2,12 +2,15 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let mut a = "Hello ".to_owned();
+        let b = "Rust!";
+        a.push_str(b);
+        assert_eq!("Hello Rust!", a);
     }
 }
