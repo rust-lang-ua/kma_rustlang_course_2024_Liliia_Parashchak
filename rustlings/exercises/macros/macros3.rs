b0c0c1003be44a6cf38e8a96354b156bd0b267aa
Liliia Parashchak <liliia.parashchak@gmail.com>
2024-10-14 23:26:14 +0300

    Macros completed

--- rustlings/exercises/macros/macros3.rs
+++ rustlings/exercises/macros/macros3.rs
@@ -2,14 +2,16 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
+// I AM DONE
 
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+
 }
 
 fn main() {
